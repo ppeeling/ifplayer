ea6daf39fd20c53f5e873ab81ad253bd7f985782
Paul Peeling <ppeeling@mathworks.com>
2026-03-20 23:23:24 +0000

    hard-code client id

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { registerSW } from 'virtual:pwa-register';
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'YOUR_GOOGLE_CLIENT_ID';
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '908087773688-ncu5gsdl6dvv1hltdm8v8ks76qnq3fm5.apps.googleusercontent.com';
 
 registerSW({ immediate: true });
 
